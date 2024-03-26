962efe3c8c0c87e29c5c18d7e8c2ca9b1a773312
shantanusingh04 <forfrontenddev@gmail.com>
2024-03-26 14:52:40 +0530

    Added details page

--- flixx-app/js/script.js
+++ flixx-app/js/script.js
@@ -37,12 +37,111 @@ async function displayPopularMovies(){
       document.querySelector('#popular-movies').appendChild(div)
     })
 }
+async function displayPopularShows(){
+    const {results}=await fetchAPIData('tv/popular')
+    results.forEach((show)=>{
+        const div=document.createElement('div')
+        div.classList.add('card')
+        div.innerHTML=`
+        <a href="movie-details.html?id=${show.id}">
+          ${
+            show.poster_path
+            ?
+            `<img
+            src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+            class="card-img-top"
+            alt="${show.name}"
+          />`
+          :
+          `<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="${show.name}"
+        />`
+          }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${show.name}</h5>
+          <p class="card-text">
+            <small class="text-muted">Release: ${show.first_air_date}</small>
+          </p>
+        </div>
+      `
+      document.querySelector('#popular-shows').appendChild(div)
+    })
+}
+
+async function displayMovieDetails(){
+    const movieId=window.location.search.split('=')[1]
+    const movie=await fetchAPIData(`movie/${movieId}`)
+    const div=document.createElement('div')
+    div.innerHTML=`
+    <div class="details-top">
+          <div>
+          ${
+            movie.poster_path
+            ?
+            `<img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.name}"
+          />`
+          :
+          `<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="${movie.name}"
+        />`
+          }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)}/ 10
+            </p>
+            <p class="text-muted">${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+            ${movie.genres.map((genre)=>`<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href=${movie.homepage} target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNum(movie.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNum(movie.revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+          ${movie.production_companies.map((company)=>`<span>${company.name}</span>`).join(', ')}
+          </div>
+        </div>
+    `
+    document.querySelector('#movie-details').appendChild(div)
+}
 async function fetchAPIData(endpoint){
     const API_KEY='b318957234b583368d2458a1c3f74ece'
     const API_URL='https://api.themoviedb.org/3/'
+    showSpinner()
     const response=await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
     const data=await response.json()
+    hideSpinner()
     return data;
+}
+
+function showSpinner(){
+    document.querySelector('.spinner').classList.add('show')
+}
+function hideSpinner(){
+    document.querySelector('.spinner').classList.remove('show')
 }
 function highlightActiveLink(){
     const links=document.querySelectorAll('.nav-link')
@@ -52,7 +151,9 @@ function highlightActiveLink(){
         }
     })
 }
-
+function addCommasToNum(num){
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 function init(){
     switch(global.currentPage){
         case '/':
@@ -61,10 +162,10 @@ function init(){
             console.log('Home')
             break;
             case '/shows.html':
-                console.log('Shows')
+                displayPopularShows()
                 break;
             case '/movie-details.html':
-                console.log('Movies')
+                displayMovieDetails()
                 break;
             case '/tv-details.html':
                 console.log('TV Shows')

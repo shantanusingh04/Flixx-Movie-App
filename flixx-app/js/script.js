ba7f782fc5aab3f6fca004afb16571d617740574
shantanusingh04 <forfrontenddev@gmail.com>
2024-04-01 16:31:26 +0530

    Added swiper slider

--- flixx-app/js/script.js
+++ flixx-app/js/script.js
@@ -208,6 +208,46 @@ if(type==='movie'){
   }
 }
 
+async function displaySlider(){
+  const {results}=await fetchAPIData('movie/now_playing')
+  results.forEach((movie)=>{
+    const div=document.createElement('div')
+    div.classList.add('swiper-slide')
+    div.innerHTML=`<a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+            </h4>`
+    document.querySelector('.swiper-wrapper').appendChild(div)
+    initSwiper()
+  })
+}
+
+function initSwiper(){
+  const swiper=new Swiper('.swiper',{
+    slidesPerView:1,
+    spaceBetween:30,
+    freeMode:true,
+    loop:true,
+    autoplay:{
+      delay:4000,
+      disableOnInteraction:false
+    },
+    breakpoints:{
+      500:{
+        slidesPerView:2
+      },
+      700:{
+        slidesPerView:3
+      },
+      1200:{
+        slidesPerView:4
+      },
+    }
+  })
+}
+
 async function fetchAPIData(endpoint){
     const API_KEY='b318957234b583368d2458a1c3f74ece'
     const API_URL='https://api.themoviedb.org/3/'
@@ -239,6 +279,7 @@ function init(){
     switch(global.currentPage){
         case '/':
             case '/index.html':
+                displaySlider()
                 displayPopularMovies()
             console.log('Home')
             break;
